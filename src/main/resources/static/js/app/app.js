ac0e7f95f774c0dfebac533f738c7dea10595eec
krzysiu26911 <227086@student.pwr.edu.pl>
2018-06-24 19:29:24 +0200

    show list of books

--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -2,13 +2,14 @@ var app = angular.module('app', []);
 
 app.controller('appController', function($scope, $http) {
     $scope.listBook = [];
-    function getAllCustomer(){
+    function getAllBooks(){
 
-        $http.get("http://localhost:8080/books").then(function (response) {
+        $http.get("http://localhost:8080/api/books").then(function (response) {
             $scope.listBook = response.data;
             console.log(response.data)
         })
     }
-    getAllCustomer()
+    getAllBooks()
+    
    
 });
